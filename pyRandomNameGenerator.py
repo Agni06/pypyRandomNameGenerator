792cd981af6d3683bf5e7b02ca9b175bfac644e7
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-05 13:01:32 +0530

    Add files via upload
    A diverse list of 100 randomly generated names for Python, combining popular first names and last names, suitable for various programming projects and simulations.

---
+++ pyRandomNameGenerator.py
@@ -0,0 +1,26 @@
+import random
+def nameGenerator():
+    names_list = [
+    "Alejandra Donovan", "Brayan Sullivan", "Melanie Owen", "Cannon Duffy", "Addisyn Carey",
+    "Watson Arroyo", "Kyra Rush", "Kaiser Campbell", "Addison Cross", "Fabian Cannon",
+    "Noa Morgan", "Hunter Rhodes", "Tatum Abbott", "Kohen Douglas", "Aniyah Schneider",
+    "Raymond Porter", "Ryleigh Montgomery", "Maximiliano Fernandez", "Amara Atkinson",
+    "Duke Dawson", "Veronica Allison", "Dennis Bryan", "Meredith Ortega", "Kobe Webster",
+    "Kensley Edwards", "Adrian Cordova", "Florence Mata", "Ray Pope", "Aurelia Barton",
+    "Cassius Marshall", "Adalyn Small", "Rudy Sparks", "Aisha Franco", "Gage Ayers",
+    "Simone Bullock", "Ben Doyle", "Annalise Jefferson", "Raylan Bean", "Jenesis Ramirez",
+    "David Nielsen", "Vienna Conrad", "Dilan Yang", "Angelina Griffin", "Ayden Ramos",
+    "Alice Landry", "Jaxx Becker", "Laura Cox", "Connor Burns", "Emerson Heath",
+    "Lionel Richard", "Davina Berger", "Byron Harvey", "Nicole O’brien", "Riley Ortega",
+    "Lilah Wang", "Cohen Ramsey", "Lyric Kelly", "Cooper Beard", "Ezra Arroyo",
+    "Alberto Owens", "Amaya Cuevas", "Brecken Petersen", "Fernanda Deleon", "Nasir Turner",
+    "Brooklyn Rivers", "Bear Erickson", "Sabrina Moses", "Niklaus Dean", "Julianna Daniels",
+    "Xander Hines", "Poppy Ellis", "Cole Molina", "Alexandria Ramirez", "David Boyle",
+    "Aliya Reid", "Josue Lucas", "Phoenix Klein", "Marco White", "Layla Briggs",
+    "Case Palacios", "Bria Esquivel", "Bridger Bentley", "Jaylin Johnston", "Felix Singh",
+    "Vivienne Giles", "Kole Wade", "Evie Bryan", "Jaxtyn Clements", "Cara Brewer",
+    "Cruz Lucero", "Ila McCall", "Kiaan Whitney", "Madalynn Aguilar", "Milo Simmons",
+    "Reagan Sexton", "Mylo Estrada", "Sawyer Patton", "Moises Burns", "Emerson Johnston",
+    "Felix Crane", "Della Harvey", "Cayden Brooks"]
+
+    print(random.choice(names_list))
